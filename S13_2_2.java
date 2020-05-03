926d6c9ae9edce53277a073047d3345e9a8641ce
KimJinHye0n <63627253+KimJinHye0n@users.noreply.github.com>
2020-05-03 11:52:35 +0900

    Update S13_2_2.java

--- S13_2_2.java
+++ S13_2_2.java
@@ -11,15 +11,15 @@ public class S13_2_2 {
 		public static void change(int[][] arr) {
 		int a = arr.length;
 		int b = arr[0].length;
-		int[][] arr2 = new int[a][b];
+		int[][] arr2 = new int[a][b]; // 대입하는 행렬의 크기 따왔음
 		for(int i = 0; i<a; i++) {
-				if (i+1 != a) {
+				if (i+1 != a) { // 마지막 행과 같지 않을 경우 0번째 행부터 새로운 행렬의 1번째 행으로 옮겨줌
 					arr2[i+1]= arr[i];
 					}	
-				else {
+				else { // i가 a(마지막행)일 경우 대입행렬의 마지막 행을 새로운 행렬 1번째 행으로 옮겨줌
 					arr2[0] = arr[i];
 				}	
 			}
-		System.out.println(Arrays.deepToString(arr2));		
+		System.out.println(Arrays.deepToString(arr2));//행렬 출력 방법		
 		}
 	}
